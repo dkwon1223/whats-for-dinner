f82571de012b4f5faba0ded3ca7de26630b4db18
David Kwon <dkwon1223@gmail.com>
2023-12-19 20:30:39 -0700

    Add arrays of string data for sides, mains, and desserts in main.js

---
+++ main.js
@@ -0,0 +1,91 @@
+// Meal data arrays:
+
+let sidesArray = [
+    "Green Beans",
+    "Zucchini and Squash",
+    "White Rice",
+    "Basmati Rice",
+    "Couscous",
+    "Roasted Potatoes",
+    "Fried Rice",
+    "Coleslaw",
+    "Lo Mein",
+    "Mac n Cheese",
+    "Stuffed Peppers",
+    "Caesar Salad",
+    "Sweet Potato Fries",
+    "Baked Potato",
+    "Wedge Salad",
+    "Miso Roasted Carrots",
+    "Casserole",
+    "Quinoa",
+    "Garlic Bread",
+    "Creamed Spinach",
+    "Cripsy Brussel Sprouts",
+    "Roasted Fennel and Apples",
+    "Honey Miso Eggplant",
+    "Mushroom Risotto",
+    "Greek Salad"
+]
+
+console.log(sidesArray.length);
+
+let mainsArray = [
+    "Cedar Plank Salmon",
+    "Herb Roasted Chicken",
+    "Seared Scallops and Brown Butter and Lemon Pan Sauce",
+    "Lasagna",
+    "Habanero Lime Grilled Shrimp Skewers",
+    "Grilled Pork Spareribs",
+    "Fish Tacos",
+    "Spicy Korean Chicken Breast",
+    "Miso Black Cod",
+    "Mediterranean Seabass",
+    "Pad Thai",
+    "Chicken Cordon Bleu",
+    "Beef Stew",
+    "Pan Seared Ribeye",
+    "Spaghetti and Meatballs",
+    "Creamy Tuscan Chicken",
+    "Honey Garlic Salmon",
+    "Pepper Steak Stir Fry",
+    "Chicken Fried Steak and Gravy",
+    "Steak Fajitas",
+    "Shrimp Scampi and Linguini",
+    "Cajun Pork Chop",
+    "Swedish Meatballs",
+    "Smoky Buffalo Wings",
+    "Classic Carbonara"
+]
+
+console.log(mainsArray.length);
+
+let dessertsArray = [
+    "Chocolate Mousse",
+    "Tiramisu",
+    "Almond and Date Cake",
+    "Creme Brulee",
+    "Lemon Meringue Pie",
+    "Banana Split",
+    "Amaretto Cheesecake",
+    "Affogato",
+    "Apple and Custard Crumble",
+    "Sticky Toffee Pudding",
+    "Banana Pudding",
+    "Bananas Foster",
+    "Double Fudge Brownies",
+    "Fried Oreos",
+    "Boston Cream Pie",
+    "Peach Cobbler",
+    "Strawberry Shortcake",
+    "Chocolate Motlen Lava Cake",
+    "Bread Pudding",
+    "Cannolis",
+    "Cinnamon Rolls",
+    "Chocolate Eclaires",
+    "Chocolate Chip Cookies",
+    "Baked Alaska",
+    "S'mores"
+]
+
+console.log(dessertsArray.length);
